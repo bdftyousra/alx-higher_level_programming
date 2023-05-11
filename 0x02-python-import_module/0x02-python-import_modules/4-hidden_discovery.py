da2d8bbb09834b3f6a55fb86e5bdfa12f43a4e43
bdftyousra <bdftyousra@gmail.com>
2023-05-11 06:58:21 -0700

    0x02. Python - import & modules

--- 0x02-python-import_module/0x02-python-import_modules/4-hidden_discovery.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-    # Print sorted name from directory
-    for name in sorted(dir(hidden_4)):
-        # print only names that do not start with __
-        if name[:2] != '__':
-            print("{}".format(name))
