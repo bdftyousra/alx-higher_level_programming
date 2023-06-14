0a422b61593edaee27e85963e8951301de74b87f
bdftyousra@gmail.com <bdftyousra@gmail.com>
2023-06-14 16:41:25 +0000

    commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+0-read_file module
+"""
 
 
 def read_file(filename=""):
+    """
+    read_file - reads a text file (UTF8) and prints it to stdout
+    Args:
+        filename: name of the file
+    """
     with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+        for line in f:
+            print(line, end="")
